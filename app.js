46902884dcf5dad9de4299015dcf9545a38b8e06
SimonasGer <159446055+SimonasGer@users.noreply.github.com>
2024-07-28 22:43:47 +0300

    cors erroras sutvarkytas

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
 const dishRouter = require("./routes/dishRoutes");
 app.use(express.json());
-
-module.exports = app;
+app.use(cors());
 
 app.use("/users", userRoutes);
 app.use("/dishes", dishRouter)
+
+module.exports = app;
